22c76b57246cdc220afeae8652f5f1d1074083fd
Glowman554 <glowman434@gmail.com>
2021-12-16 06:35:30 +0100

    added .. support

--- stdlib/environment.c
+++ stdlib/environment.c
@@ -18,6 +18,8 @@ char* getenv(const char* name) {
     return NULL;
 }
 
+void compute_dot_dot(char* path, char* output);
+
 void resolve(char* path, char* output) {
 	char* cwd = (char*) env(ENV_GET_CWD);
 	memcpy(output, cwd, strlen(cwd));
@@ -47,5 +49,59 @@ void resolve(char* path, char* output) {
 	} else {
 		memset(output, 0, sizeof(output));
 		strcpy(output, tmp);
+	}
+
+	char compute_dot_dot_path[256];
+	memset(compute_dot_dot_path, 0, sizeof(compute_dot_dot_path));
+	strcpy(compute_dot_dot_path, output);
+
+	compute_dot_dot(compute_dot_dot_path, output);
+}
+
+void compute_dot_dot(char* path, char* output) {
+	// example input fat32_0:/bin/..
+	// example output fat32_0:/
+	// example input fat32_0:/efi/foxos/../boot
+	// example output fat32_0:/efi/foxos/boot
+
+	char* path_segments[256];
+	int path_segments_count = 0;
+	memset(path_segments, 0, sizeof(path_segments));
+
+	int path_len = strlen(path);
+	for (int i = 0; i < path_len; i++) {
+		if (path[i] == '/') {
+			path[i] = '\0';
+			path_segments[path_segments_count++] = &path[i + 1];
+		}
+	}
+
+	
+
+	for (int i = 0; i < path_segments_count; i++) {
+		if (strcmp(path_segments[i], "..") == 0) {
+			if (i == 0) {
+				printf("Unsupported path: '%s'\n", path);
+				return;
+			}
+
+			int del_idx = i - 1;
+			while(del_idx >= 0 && path_segments[del_idx] == NULL) {
+				del_idx--;
+			}
+
+			path_segments[del_idx] = NULL;
+			path_segments[i] = NULL;
+		}
+	}
+
+	memset(output, 0, 256);
+	strcpy(output, strtok(path, "/"));
+
+	for (int i = 0; i < path_segments_count; i++) {
+		if (path_segments[i] != NULL) {
+			strcat(output, "/");
+			strcat(output, path_segments[i]);
+		}
 	}
 }
