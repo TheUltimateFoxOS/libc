79d6ba0c9d40cd56194d82412b53da52b06a69c4
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2021-12-18 08:33:48 +0100

    Added a unix time to string function

--- include/buildin/unix_time.h
+++ include/buildin/unix_time.h
@@ -4,3 +4,4 @@
 
 EXPOSEC long long to_unix_time(int year, int month, int day, int hour, int minute, int second);
 EXPOSEC void from_unix_time(unsigned long int unixtime, int* year, int* month, int* day, int* hour, int* minute, int* second);
+EXPOSEC void unix_time_to_string(unsigned long int unixtime, char* buffer);
