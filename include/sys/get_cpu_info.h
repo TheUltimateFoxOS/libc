eca00a2a0f3cc3040748a4260ddc2d96e5e3897a
Glowman554 <glowman434@gmail.com>
2022-05-03 11:52:49 +0200

    added cpu info

---
+++ include/sys/get_cpu_info.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdint.h>
+#include <extern.h>
+
+typedef struct {
+	uint64_t id;
+	int speed;
+	char manufacture[64];
+	char version[64];
+} cpu_info_t;
+
+EXPOSEC int get_cpu_info(cpu_info_t* cpu_info);
