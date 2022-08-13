2f4c22b0c16986959b3b9d39f3c1579ee18a42ca
Glowman554 <glowman434@gmail.com>
2022-08-13 23:55:10 +0200

    added c++ list

--- include/buildin/list.h
+++ include/buildin/list.h
@@ -18,3 +18,79 @@ EXPOSEC struct list_node_t* __libc_list_remove_front(struct list_node_t* head);
 EXPOSEC struct list_node_t* __libc_list_remove_back(struct list_node_t* head);
 EXPOSEC struct list_node_t* __libc_list_remove(struct list_node_t* head, struct list_node_t* nd);
 EXPOSEC void __libc_list_dispose(struct list_node_t* head);
+
+#ifdef __cplusplus
+#pragma once
+
+#include <string.h>
+#include <stdlib.h>
+
+template <typename T>
+class list {
+	public:
+
+		struct node {
+			T data;
+			bool taken = false;
+		};
+
+		list(int initial_length) {
+			length = initial_length;
+			data = (node*) malloc((length + 1) * sizeof(node));
+			memset(data, 0, (length + 1) * sizeof(node));
+		};
+
+		~list() {
+			free(data);
+		}
+
+		template <typename Y>
+		node* find(bool (*f)(Y, node*), Y arg) {
+			for (size_t i = 0; i < length; i++) {
+				if (data[i].taken) {
+					if (f(arg, &data[i])) {
+						return &data[i];
+					}
+				}
+			}
+			
+			return nullptr;
+		}
+
+		template <typename Y>
+		void foreach(void (*f)(Y, node*), Y arg) {
+			for (int i = 0; i < length; i++) {
+				if (data[i].taken) {
+					f(arg, &data[i]);
+				}
+			}
+		}
+
+		void remove(node* n) {
+			for (size_t i = 0; i < length; i++) {
+				if (&data[i] == n) {
+					memset(&data[i], 0, sizeof(node));
+				}
+			}
+		}
+
+		node* add(T d) {
+		retry:
+			for (int i = 0; i < length; i++) {
+				if (!data[i].taken) {
+					data[i].data = d;
+					data[i].taken = true;
+					return &data[i];
+				}
+			}
+
+			data = (node*) realloc(data, (length + 2) * sizeof(node));
+			memset(&data[length], 0, sizeof(node));
+			length++;
+			goto retry;
+		}
+	
+		int length;
+		node* data;
+};
+#endif
